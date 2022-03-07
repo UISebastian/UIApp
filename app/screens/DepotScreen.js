2c7957694541e263ab0e3c5f19d118a0a6719a62
UISebastian <Sebastian-Marcus.Meier@union-investment.de>
2022-03-07 10:11:56 +0100

    DepotScreen now working

--- app/screens/DepotScreen.js
+++ app/screens/DepotScreen.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import WebView from 'react-native-webview';
+
+import colors from '../config/colors';
 
 function DepotScreen(props) {
     return (
-        <View>
+        
+            <WebView style={styles.background} onLoad={() => console.log("Loaded")} source={{uri: 'https://service.meindepot.union-investment.de/auth/realms/union-investment/protocol/openid-connect/auth?client_id=dashboard-frontend&redirect_uri=https%3A%2F%2Fanlegen.meindepot.union-investment.de%2F%2Fdashboard&state=f7d7f17a-c196-4922-879f-87757fe4ce04&response_mode=fragment&response_type=code&scope=openid&nonce=2daf6030-f091-49a0-8069-9381687f425c&code_challenge=87Z1TgusbEGz8Kw-lWOZlLR-PyowHYw6myTsCQhNSpY&code_challenge_method=S256'}}>
+
+            </WebView>
             
-        </View>
+        
     );
 }
-
+const styles = StyleSheet.create({
+    background:{
+        
+        alignItems:'center',
+        justifyContent:'flex-end',
+        flex:1
+        
+    },
+})
 export default DepotScreen;
